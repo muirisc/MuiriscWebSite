d35fd027f5a1908af15a9d6a5e9e23c76aa908b6
muirisc <91299876+muirisc@users.noreply.github.com>
2022-05-25 22:01:43 -0400

    Files

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import "../CSS/Footer.css";
+
+const Footer = () => {
+
+
+  return(
+    <div className="footer" >
+      <div>
+        <p>©2022 By Michael Vanaman</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
